e9ab897d8b25818323a8de429fd783521c5432b8
TheRandomLabs <therandomlabsinc@gmail.com>
2017-12-25 13:15:59 +1100

    Reformat comment

--- src/main/java/com/therandomlabs/curseapi/widget/DateInfo.java
+++ src/main/java/com/therandomlabs/curseapi/widget/DateInfo.java
@@ -2,8 +2,8 @@ package com.therandomlabs.curseapi.widget;
 
 public final class DateInfo implements Cloneable {
 	public String date;
-	/* https://stackoverflow.com/questions/17694894/different-timezone-
-	types-on-datetime-object/17711005#17711005 */
+	//https://stackoverflow.com/questions/17694894/different-timezone-types-on-datetime-object/
+	//17711005#17711005
 	public int timezone_type;
 	public String timezone;
 
