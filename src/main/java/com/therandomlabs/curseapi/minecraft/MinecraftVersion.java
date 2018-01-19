3f67889db5d5c83345c42ce68adee67c69589ec9
TheRandomLabs <therandomlabsinc@gmail.com>
2018-01-20 00:13:56 +1100

    ALL the Minecraft versions

--- src/main/java/com/therandomlabs/curseapi/minecraft/MinecraftVersion.java
+++ src/main/java/com/therandomlabs/curseapi/minecraft/MinecraftVersion.java
@@ -19,36 +19,82 @@ public enum MinecraftVersion {
 	V1_12_1(V1_12_GROUP),
 	@SerializedName("1.12")
 	V1_12(V1_12_GROUP),
+	@SerializedName("1.12-Snapshot")
+	V1_12_SNAPSHOT(V1_12_GROUP),
 	@SerializedName("1.11-Group")
 	V1_11_GROUP,
 	@SerializedName("1.11.2")
 	V1_11_2(V1_11_GROUP),
+	@SerializedName("1.11.1")
+	V1_11_1(V1_11_GROUP),
 	@SerializedName("1.11")
 	V1_11(V1_11_GROUP),
+	@SerializedName("1.11-Snapshot")
+	V1_11_SNAPSHOT(V1_11_GROUP),
 	@SerializedName("1.10-Group")
 	V1_10_GROUP,
 	@SerializedName("1.10.2")
 	V1_10_2(V1_10_GROUP),
+	@SerializedName("1.10.1")
+	V1_10_1(V1_10_GROUP),
 	@SerializedName("1.10")
 	V1_10(V1_10_GROUP),
+	@SerializedName("1.10-Snapshot")
+	V1_10_SNAPSHOT(V1_10_GROUP),
 	@SerializedName("1.9-Group")
 	V1_9_GROUP,
 	@SerializedName("1.9.4")
 	V1_9_4(V1_9_GROUP),
+	@SerializedName("1.9.3")
+	V1_9_3(V1_9_GROUP),
+	@SerializedName("1.9.2")
+	V1_9_2(V1_9_GROUP),
+	@SerializedName("1.9.1")
+	V1_9_1(V1_9_GROUP),
 	@SerializedName("1.9")
 	V1_9(V1_9_GROUP),
+	@SerializedName("1.9-Snapshot")
+	V1_9_SNAPSHOT(V1_9_GROUP),
 	@SerializedName("1.8-Group")
 	V1_8_GROUP,
 	@SerializedName("1.8.9")
 	V1_8_9(V1_8_GROUP),
 	@SerializedName("1.8.8")
 	V1_8_8(V1_8_GROUP),
+	@SerializedName("1.8.7")
+	V1_8_7(V1_8_GROUP),
+	@SerializedName("1.8.6")
+	V1_8_6(V1_8_GROUP),
+	@SerializedName("1.8.5")
+	V1_8_5(V1_8_GROUP),
+	@SerializedName("1.8.4")
+	V1_8_4(V1_8_GROUP),
+	@SerializedName("1.8.3")
+	V1_8_3(V1_8_GROUP),
+	@SerializedName("1.8.2")
+	V1_8_2(V1_8_GROUP),
+	@SerializedName("1.8.1")
+	V1_8_1(V1_8_GROUP),
 	@SerializedName("1.8")
 	V1_8(V1_8_GROUP),
+	@SerializedName("1.8-Snapshot")
+	V1_8_SNAPSHOT(V1_8_GROUP),
 	@SerializedName("1.7-Group")
 	V1_7_GROUP,
 	@SerializedName("1.7.10")
 	V1_7_10(V1_7_GROUP),
+	@SerializedName("1.7.9")
+	V1_7_9(V1_7_GROUP),
+	@SerializedName("1.7.8")
+	V1_7_8(V1_7_GROUP),
+	@SerializedName("1.7.7")
+	V1_7_7(V1_7_GROUP),
+	@SerializedName("1.7.6")
+	V1_7_6(V1_7_GROUP),
+	@SerializedName("1.7.5")
+	V1_7_5(V1_7_GROUP),
+	@SerializedName("1.7.4")
+	V1_7_4(V1_7_GROUP),
 	@SerializedName("1.7.2")
 	V1_7_2(V1_7_GROUP),
 	@SerializedName("1.6-Group")
@@ -83,6 +129,8 @@ public enum MinecraftVersion {
 	V1_3_GROUP,
 	@SerializedName("1.3.2")
 	V1_3_2(V1_3_GROUP),
+	@SerializedName("1.3.1")
+	V1_3_1(V1_3_GROUP),
 	@SerializedName("1.2-Group")
 	V1_2_GROUP,
 	@SerializedName("1.2.5")
@@ -91,10 +139,18 @@ public enum MinecraftVersion {
 	V1_2_4(V1_2_GROUP),
 	@SerializedName("1.2.3")
 	V1_2_3(V1_2_GROUP),
+	@SerializedName("1.2.2")
+	V1_2_2(V1_2_GROUP),
+	@SerializedName("1.2.1")
+	V1_2_1(V1_2_GROUP),
 	@SerializedName("1.1-Group")
 	V1_1_GROUP,
 	@SerializedName("1.1")
-	V1_1(V1_1_GROUP);
+	V1_1(V1_1_GROUP),
+	@SerializedName("1.0-Group")
+	V1_0_GROUP,
+	@SerializedName("1.0.0")
+	V1_0_0(V1_0_GROUP);
 
 	private final String versionString;
 	private final TRLList<MinecraftVersion> versions = new TRLList<>(3);
@@ -111,7 +167,7 @@ public enum MinecraftVersion {
 	MinecraftVersion() {
 		this.group = this;
 		this.versionString = super.toString().substring(1).replaceAll("_", ".").
-				replaceAll(".GROUP", "-Group");
+				replaceAll(".GROUP", "-Group").replaceAll(".SNAPSHOT", "-Snapshot");
 	}
 
 	public TRLList<MinecraftVersion> getVersions() {
