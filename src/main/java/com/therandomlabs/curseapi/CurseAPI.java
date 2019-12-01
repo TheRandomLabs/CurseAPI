df588d88ad88014761cc308f818d35f09c87ed82
TheRandomLabs <therandomlabsinc@gmail.com>
2019-12-01 15:47:59 +1100

    Start Javadocs

--- src/main/java/com/therandomlabs/curseapi/CurseAPI.java
+++ src/main/java/com/therandomlabs/curseapi/CurseAPI.java
@@ -21,11 +21,17 @@ import org.slf4j.LoggerFactory;
  * as for managing {@link CurseAPIProvider}s.
  */
 public final class CurseAPI {
+	/**
+	 * The placeholder CurseForge project avatar URL.
+	 */
 	public static final HttpUrl PLACEHOLDER_PROJECT_AVATAR = HttpUrl.get(
 			"https://www.curseforge.com/Content/2-0-7263-28137/Skins/Elerium/images/icons/" +
 					"avatar-flame.png"
 	);
 
+	/**
+	 * The placeholder CurseForge project thumbnail URL.
+	 */
 	public static final HttpUrl PLACEHOLDER_PROJECT_THUMBNAIL =
 			HttpUrl.get("https://media.forgecdn.net/avatars/0/93/635227964539626926.png");
 
@@ -36,22 +42,56 @@ public final class CurseAPI {
 
 	private CurseAPI() {}
 
+	/**
+	 * Returns a {@link CurseProject} instance for the specified project ID.
+	 *
+	 * @param id a project ID.
+	 * @return a {@link CurseProject} instance for the specified project ID wrapped in an
+	 * {@link Optional} if the project exists, or otherwise {@link Optional#empty()}}.
+	 * @throws CurseException if an error occurs.
+	 */
 	public static Optional<CurseProject> project(int id) throws CurseException {
 		Preconditions.checkArgument(id >= 10, "id should not be smaller than 10");
 		return get(provider -> provider.project(id));
 	}
 
+	/**
+	 * Returns a {@link CurseFiles} instance for the specified project ID.
+	 *
+	 * @param projectID a project ID.
+	 * @return a {@link CurseFiles} instance for the specified project ID wrapped in an
+	 * {@link Optional} if the project exists, or otherwise {@link Optional#empty()}}.
+	 * @throws CurseException if an error occurs.
+	 */
 	public static Optional<CurseFiles> files(int projectID) throws CurseException {
 		Preconditions.checkArgument(projectID >= 10, "projectID should not be smaller than 10");
 		return get(provider -> provider.files(projectID));
 	}
 
+	/**
+	 * Returns a {@link CurseFile} instance for the specified project and file ID.
+	 *
+	 * @param projectID a project ID.
+	 * @param fileID a file ID.
+	 * @return a {@link CurseFile} instance for the specified project and file ID wrapped in an
+	 * {@link Optional} if the file exists, or otherwise {@link Optional#empty()}}.
+	 * @throws CurseException if an error occurs.
+	 */
 	public static Optional<CurseFile> file(int projectID, int fileID) throws CurseException {
 		Preconditions.checkArgument(projectID >= 10, "projectID should not be smaller than 10");
 		Preconditions.checkArgument(fileID >= 10, "fileID should not be smaller than 10");
 		return get(provider -> provider.file(projectID, fileID));
 	}
 
+	/**
+	 * Returns the download URL for the specified project and file ID.
+	 *
+	 * @param projectID a project ID.
+	 * @param fileID a file ID.
+	 * @return the download URL for the specified project and file ID wrapped in an
+	 * {@link Optional} if the file exists, or otherwise {@link Optional#empty()}}.
+	 * @throws CurseException if an error occurs.
+	 */
 	public static Optional<HttpUrl> fileDownloadURL(int projectID, int fileID)
 			throws CurseException {
 		Preconditions.checkArgument(projectID >= 10, "projectID should not be smaller than 10");
@@ -59,6 +99,10 @@ public final class CurseAPI {
 		return get(provider -> provider.fileDownloadURL(projectID, fileID));
 	}
 
+	/**
+	 * Registers a {@link CurseAPIProvider}.
+	 * @param provider a {@link CurseAPIProvider} instance.
+	 */
 	public static void addProvider(CurseAPIProvider provider) {
 		Preconditions.checkNotNull(provider, "provider should not be null");
 		Preconditions.checkArgument(
@@ -67,11 +111,21 @@ public final class CurseAPI {
 		providers.add(0, provider);
 	}
 
+	/**
+	 * Unregisters a {@link CurseAPIProvider}.
+	 * @param provider a {@link CurseAPIProvider} instance.
+	 * @return {@code true} if the {@link CurseAPIProvider} was registered,
+	 * or otherwise {@code false}.
+	 */
 	public static boolean removeProvider(CurseAPIProvider provider) {
 		Preconditions.checkNotNull(provider, "provider should not be null");
 		return providers.remove(provider);
 	}
 
+	/**
+	 * Returns an immutable {@link List} of all registered {@link CurseAPIProvider}s.
+	 * @return an immutable {@link List} of all registered {@link CurseAPIProvider}s.
+	 */
 	public static List<CurseAPIProvider> getProviders() {
 		return ImmutableList.copyOf(providers);
 	}
--- src/main/java/com/therandomlabs/curseapi/CurseAPIProvider.java
+++ src/main/java/com/therandomlabs/curseapi/CurseAPIProvider.java
@@ -4,12 +4,48 @@ import com.therandomlabs.curseapi.file.CurseFile;
 import com.therandomlabs.curseapi.file.CurseFiles;
 import okhttp3.HttpUrl;
 
+/**
+ * Provides an implementation of all or a subset of CurseAPI.
+ * <p>
+ * Implementations of this interface may return {@code null} to signify that a fallback
+ * {@link CurseAPIProvider} should be used instead.
+ */
 public interface CurseAPIProvider {
+	/**
+	 * Returns a {@link CurseProject} instance for the specified project ID.
+	 *
+	 * @param id a project ID.
+	 * @return a {@link CurseProject} instance for the specified project ID.
+	 * @throws CurseException if an error occurs.
+	 */
 	CurseProject project(int id) throws CurseException;
 
+	/**
+	 * Returns a {@link CurseFiles} instance for the specified project ID.
+	 *
+	 * @param projectID a project ID.
+	 * @return a {@link CurseFiles} instance for the specified project ID.
+	 * @throws CurseException if an error occurs.
+	 */
 	CurseFiles files(int projectID) throws CurseException;
 
+	/**
+	 * Returns a {@link CurseFile} instance for the specified project and file ID.
+	 *
+	 * @param projectID a project ID.
+	 * @param fileID a file ID.
+	 * @return a {@link CurseFile} instance for the specified project and file ID.
+	 * @throws CurseException if an error occurs.
+	 */
 	CurseFile file(int projectID, int fileID) throws CurseException;
 
+	/**
+	 * Returns the download URL for the specified project and file ID.
+	 *
+	 * @param projectID a project ID.
+	 * @param fileID a file ID.
+	 * @return the download URl for the specified project and file ID.
+	 * @throws CurseException if an error occurs.
+	 */
 	HttpUrl fileDownloadURL(int projectID, int fileID) throws CurseException;
 }
