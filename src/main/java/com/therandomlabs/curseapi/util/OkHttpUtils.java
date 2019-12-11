fb397ec268b6c798eb6a51f3974fc07141c6b45f
TheRandomLabs <therandomlabsinc@gmail.com>
2019-12-11 11:05:48 +1100

    OkHttpUtils#download(HttpUrl)

--- src/main/java/com/therandomlabs/curseapi/util/OkHttpUtils.java
+++ src/main/java/com/therandomlabs/curseapi/util/OkHttpUtils.java
@@ -2,6 +2,7 @@ package com.therandomlabs.curseapi.util;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.nio.file.Path;
 
 import javax.imageio.ImageIO;
 
@@ -10,6 +11,8 @@ import com.therandomlabs.curseapi.CurseException;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okio.BufferedSink;
+import okio.Okio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,15 +30,37 @@ public final class OkHttpUtils {
 	 *
 	 * @param url an image URL.
 	 * @return a {@link BufferedImage} read from the specified URL.
-	 * @throws CurseException if the request could not be executed correctly.
+	 * @throws CurseException if the request cannot be executed correctly.
 	 */
 	public static BufferedImage readImage(HttpUrl url) throws CurseException {
 		Preconditions.checkNotNull(url, "url should not be null");
 
+		final Request request = new Request.Builder().url(url).build();
+		logger.info("Executing request: {}", request);
+
 		try {
-			final Request request = new Request.Builder().url(url).build();
-			logger.info("Executing request: {}", request);
 			return ImageIO.read(client.newCall(request).execute().body().byteStream());
+		} catch (IOException ex) {
+			throw new CurseException(ex);
+		}
+	}
+
+	/**
+	 * Downloads a file from the specified {@link HttpUrl} to the specified {@link Path}.
+	 *
+	 * @param url a {@link HttpUrl}.
+	 * @param path a {@link Path}.
+	 * @throws CurseException if the request cannot be executed correctly.
+	 */
+	public static void download(HttpUrl url, Path path) throws CurseException {
+		Preconditions.checkNotNull(url, "url should not be null");
+		Preconditions.checkNotNull(path, "path should not be null");
+
+		final Request request = new Request.Builder().url(url).build();
+		logger.info("Executing request: {}", request);
+
+		try (BufferedSink sink = Okio.buffer(Okio.sink(path))) {
+			sink.writeAll(client.newCall(request).execute().body().source());
 		} catch (IOException ex) {
 			throw new CurseException(ex);
 		}
