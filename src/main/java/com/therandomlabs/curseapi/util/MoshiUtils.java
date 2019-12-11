9fb733a62e42f9e5bc3ee60859d185e1aa42d242
TheRandomLabs <therandomlabsinc@gmail.com>
2019-12-11 11:48:43 +1100

    Moshi utility methods

--- src/main/java/com/therandomlabs/curseapi/util/MoshiUtils.java
+++ src/main/java/com/therandomlabs/curseapi/util/MoshiUtils.java
@@ -1,9 +1,15 @@
 package com.therandomlabs.curseapi.util;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 import com.squareup.moshi.Moshi;
+import com.therandomlabs.curseapi.CurseException;
+import okio.BufferedSource;
+import okio.Okio;
 
 /**
- * Contains {@link #MOSHI}.
+ * Contains utility methods for working with Moshi.
  */
 public final class MoshiUtils {
 	/**
@@ -17,4 +23,38 @@ public final class MoshiUtils {
 			build();
 
 	private MoshiUtils() {}
+
+	/**
+	 * Parses the specified JSON string.
+	 *
+	 * @param json a JSON string.
+	 * @param type the {@link Class} of the type.
+	 * @param <T> the type.
+	 * @return an object with the specified type.
+	 * @throws CurseException if an error occurs.
+	 */
+	public static <T> T fromJSON(String json, Class<T> type) throws CurseException {
+		try {
+			return MOSHI.adapter(type).fromJson(json);
+		} catch (IOException ex) {
+			throw new CurseException(ex);
+		}
+	}
+
+	/**
+	 * Parses the specified JSON file.
+	 *
+	 * @param json a {@link Path} to a JSON file.
+	 * @param type the {@link Class} of the type.
+	 * @param <T> the type.
+	 * @return an object with the specified type.
+	 * @throws CurseException if an error occurs.
+	 */
+	public static <T> T fromJSON(Path json, Class<T> type) throws CurseException {
+		try (BufferedSource source = Okio.buffer(Okio.source(json))) {
+			return fromJSON(source.readUtf8(), type);
+		} catch (IOException ex) {
+			throw new CurseException(ex);
+		}
+	}
 }
